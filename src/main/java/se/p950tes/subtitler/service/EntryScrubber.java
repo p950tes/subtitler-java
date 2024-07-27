1cc471d7e5530fe6cd306071892e9c91c0aabbf9
p950tes <tobiasericsson90@hotmail.com>
2024-07-27 20:03:39 +0200

    Added more entries to scrub

--- src/main/java/se/p950tes/subtitler/service/EntryScrubber.java
+++ src/main/java/se/p950tes/subtitler/service/EntryScrubber.java
@@ -14,14 +14,20 @@ public class EntryScrubber {
     // (SCREAM) [SCREAM] {SCREAM}
     private static final Pattern BRACKET_PATTERN = Pattern.compile("[\\[\\{\\(][^\\[\\]\\{\\}\\(\\)]*[\\]\\}\\)]");
     
-    // music junk artifacts (J“ / j“ / [M1  )
-    private static final Pattern JUNK_PATTERN = Pattern.compile("(^([jJ]“)+|([jJ]“|)+$|^\\[M1$)");
+    // junk artifacts (J“ / j“ / [M1 )
+    private static final Pattern JUNK_PATTERN = Pattern.compile("("
+	    		+ "^([jJ]“)+"
+	    		+ "|([jJ]“)+$"
+	    		+ "|^\\[M1$"
+	    		+ "|^[\\-\\.:]+$"
+	    		+ "|^[ ♪]+$"
+    		+ ")");
     
     // All caps, at least 3 characters
     private static final Pattern ALL_CAPS_PATTERN = Pattern.compile("^[A-Z ,\\!]{3,}$");
     
     // Names followed by colon: (Guard 1: Hello there)
-    private static final Pattern VOICE_INDICATORS_PATTERN = Pattern.compile("^[A-Za-z]{2}[A-Za-z0-9 ]*\\s?: ");
+    private static final Pattern VOICE_INDICATORS_PATTERN = Pattern.compile("^[\\- ]*[A-Za-z]{2}[A-Za-z0-9 \']*\\s?: *");
 
     
     public void scrub(SubtitleEntry entry) {
@@ -30,6 +36,7 @@ public class EntryScrubber {
         linesAsString = scrubEntry(linesAsString);
 
         var newLines = Arrays.stream(linesAsString.split("\n"))
+        		.map(this::scrubEntry)
         		.map(String::trim)
         		.filter(StringUtils::isNotBlank)
         		.toList();
@@ -44,6 +51,7 @@ public class EntryScrubber {
 
         entry = removeAll(VOICE_INDICATORS_PATTERN, entry);
         entry = removeAll(ALL_CAPS_PATTERN, entry);
+        entry = removeAll(JUNK_PATTERN, entry);
         return entry;
     }
 
